ccd7374157a892a0940ba0cc1796fa0009974db2
Seokyong Shin <kistssy@gmail.com>
2023-11-07 13:42:09 +0900

    Update make_shortcut.py

--- make_shortcut.py
+++ make_shortcut.py
@@ -1,8 +1,12 @@
+import os
+import random
+
+while True:
+    file_name = f'link_{str(random.randint(0, 100000)).zfill(6)}.url'
+    if not os.path.exists(file_name):
+        break
+
 url = input('URL 입력: ')
-title = input('바로가기 파일 이름 입력: ')
-
-with open(title + '.url', 'w') as f:
+with open(file_name, 'w') as f:
     f.write('[InternetShortcut]\n')
-    f.write('URL=' + url)
-
-print('바로가기 생성완료.')
+    f.write(f'URL={url}')
